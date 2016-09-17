9f5c8a38271c247e0cf975359b158cd94eb9892b
nztomas <tomas.adamek@espr.it>
2016-09-17 18:22:17 +1200

    Updated inject & mvc libs

--- src/main/java/it/espr/mvc/gae/demo/WebConfig.java
+++ src/main/java/it/espr/mvc/gae/demo/WebConfig.java
@@ -5,31 +5,31 @@ import it.espr.mvc.Configuration;
 public class WebConfig extends Configuration {
 
 	@Override
-	protected void configureRoutes() {
+	protected void configureMvc() {
 		
 		// all GET requests to url /call are being routed to SimpleModel method "call"
-		this.addRoute("/call", SimpleModel.class, "call");
+		route().get("/call").to(SimpleModel.class, "call");
 
 		// all GET requests to url /get are being routed to SimpleModel method "get" and pass request parameter "id"
-		this.addRoute("/get", "get", SimpleModel.class, "get", new String[] { "id" });
+		route().get("/get").to(SimpleModel.class, "get").with("id");
 		
 		// all POST requests to url /post are being routed to SimpleModel method "post" and pass request parameters "id" & "data"
-		this.addRoute("/post", "post", SimpleModel.class, "post", new String[] { "id", "data" });
+		route().post("/post").to(SimpleModel.class, "post").with("id", "data");
 
 		// all POST requests to url /upload are being routed to SimpleModel method "upload" and pass request parameter "data"
-		this.addRoute("/upload", "post", SimpleModel.class, "upload", new String[] { "data" });
+		route().post("/upload").to(SimpleModel.class, "upload").with("data");
 
 		// similar as above just the ModelWithInjections contains @Injected bean
-		this.addRoute("/bean/call", ModelWithInjections.class, "call");
-		this.addRoute("/bean/get", "get", ModelWithInjections.class, "get", new String[] { "id" });
-		this.addRoute("/bean/post", "post", ModelWithInjections.class, "post", new String[] { "id", "data" });
-		this.addRoute("/bean/upload", "post", ModelWithInjections.class, "upload", new String[] { "data" });
+		route().get("/bean/call").to(ModelWithInjections.class, "call");
+		route().get("/bean/get").to(ModelWithInjections.class, "get").with("id");
+		route().post("/bean/post").to(ModelWithInjections.class, "post").with("id", "data");
+		route().post("/bean/upload").to(ModelWithInjections.class, "upload").with("data");
 
 		// path variables examples
 		// url "/342435/add" will call ModelWithPathAttributes method "parse" with parameters "id=342435" & "type=add"
-		this.addRoute("/([0-9]+:id)/(:type)", "get", ModelWithPathAttributes.class, "parse", new String[] { "filter" });
+		route().get("/([0-9]+:id)/(:type)").to(ModelWithPathAttributes.class, "parse").with("filter");
 		
 		// similar to above, but this time we pass path variables "id=342435" & "type=add" plus request parameter "filter"
-		this.addRoute("/o/([0-9]+:id)/(:type)", "get", ModelWithPathAttributes.class, "parseObject", new String[] { "filter" });
+		route().get("/o/([0-9]+:id)/(:type)").to(ModelWithPathAttributes.class, "parseObject").with("filter");
 	}
 }
